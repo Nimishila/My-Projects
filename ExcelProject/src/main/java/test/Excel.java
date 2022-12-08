9417cdfdef881279a6697c4969957fa733ab23ea
Nimishila <119873814+Nimishila@users.noreply.github.com>
2022-12-08 08:39:14 +0530

    Committing excel project

---
+++ ExcelProject/src/main/java/test/Excel.java
@@ -0,0 +1,32 @@
+package test;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Excel {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		FileInputStream f=new FileInputStream(new File("D:\\Student.xlsx"));
+		XSSFWorkbook wb=new XSSFWorkbook(f); 
+		XSSFSheet sheet=wb.getSheetAt(0); 
+		for(Row row: sheet)     //iteration over row using for each loop  
+		{  
+		for(Cell cell: row)    //iteration over cell using for each loop  
+		{  System.out.print(cell+"\t\n");
+		
+	
+		}
+		
+		
+		}
+
+	}
+
+}
---
+++ ExcelProject/src/main/java/test/ExcelWrite.java
@@ -0,0 +1,53 @@
+package test;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelWrite {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		@SuppressWarnings("resource")
+		XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet1 = workbook.createSheet("Java Books");
+         
+        Object[][] bookData = {
+                {"Head First Java", "Kathy Serria", 79},
+                {"Effective Java", "Joshua Bloch", 36},
+                {"Clean Code", "Robert martin", 42},
+                {"Thinking in Java", "Bruce Eckel", 35},
+        };
+ 
+        int rowCount = 0;
+         
+        for (Object[] aBook : bookData) {
+            Row row1 = sheet1.createRow(++rowCount);
+             
+            int columnCount = 0;
+             
+            for (Object field : aBook) {
+                Cell cell = row1.createCell(++columnCount);
+                if (field instanceof String) {
+                    cell.setCellValue((String) field);
+                } else if (field instanceof Integer) {
+                    cell.setCellValue((Integer) field);
+                }
+            }
+             
+        }
+         
+         
+        try (FileOutputStream outputStream = new FileOutputStream("D:\\JavaBooks.xlsx")) {
+            workbook.write(outputStream);
+        }
+        System.out.println("file created");
+    }
+
+
+	}
+
